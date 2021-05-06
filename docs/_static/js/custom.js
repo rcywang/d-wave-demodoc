a7a595756dee007f5fda68f95bce089c746e53b1
rcywang <49621326+rcywang@users.noreply.github.com>
2021-05-06 16:30:16 -0700

    Create custom.js

---
+++ docs/_static/js/custom.js
@@ -0,0 +1,59 @@
+const clientId = "e800d12fc12c4d60960778b2bc4370af";
+const viewerOptions = {
+    embedMode: "LIGHT_BOX",
+    defaultViewMode: "FIT_PAGE",
+    showDownloadPDF: false,
+    showPrintPDF: false
+};
+
+function fetchPDF(urlToPDF) {
+    return new Promise((resolve) => {
+        fetch(urlToPDF)
+            .then((resolve) => resolve.blob())
+            .then((blob) => {
+                resolve(blob.arrayBuffer());
+            })
+    })
+}
+
+function showPDF(urlToPDF) {
+    var adobeDCView = new AdobeDC.View({
+            clientId: clientId
+        });
+        var previewFilePromise = adobeDCView.previewFile(
+            {
+                content: { promise: fetchPDF(urlToPDF) },
+                metaData: { fileName: urlToPDF.split("/").slice(-1)[0] }
+            },
+            viewerOptions
+        );
+}
+
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    document.getElementById("showPDF01").addEventListener("click", function () {
+        showPDF("https://dl.dropboxusercontent.com/s/3jb1qrfrmxwgvgq/StatementofIntent_AcademicGoals_RachelWang.pdf?dl=0")
+    });
+    document.getElementById("showPDF02").addEventListener("click", function () {
+        showPDF("https://dl.dropboxusercontent.com/s/8ya3urr4mxtgyue/QDG_Notes_RachelWang.pdf?dl=0")
+    });
+    document.getElementById("showPDF03").addEventListener("click", function () {
+        showPDF("https://dl.dropboxusercontent.com/s/3jslattisxo78h8/MLOOpaper_RachelWang.pdf?dl=0")
+    });
+});
+
+// Add arrayBuffer if necessary i.e. Safari
+(function () {
+    if (Blob.arrayBuffer != "function") {
+        Blob.prototype.arrayBuffer = myArrayBuffer;
+    }
+
+    function myArrayBuffer() {
+        return new Promise((resolve) => {
+            let fileReader = new FileReader();
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+            fileReader.readAsArrayBuffer(this);
+        });
+    }
+})();
